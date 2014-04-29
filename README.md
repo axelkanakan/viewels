df96208769f221e84444d4f6609a3eb042c581eb
krillen <okulby@yahoo.com>
2014-04-29 03:21:51 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Viewels
+
+Viewels is an application that allows you to save youtube, dailymotion, and vimeo videos in one place and share it with your friends.
+
+Made by krillencoder
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viewels::Application.config.secret_token = '94a92e595731c163c95b6489e7fdf96a1963ae17745202fde538f9d6c7ae21c9c5829542ef1665dcce60d85df224ce3c07c06bbebd813d31802387be846a7807'
